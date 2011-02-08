ec988bb98e535fc0ef67deccea378055a69d0080
jrburke <jrburke@gmail.com>
2011-02-08 13:06:42 -0800

    first attempt at dynamic generation of pages. but has mystery 20 pixels offset needed and the column separator in first page does not match the other pages.

--- js/book.js
+++ js/book.js
@@ -1,83 +1,179 @@
-$(document).ready(function () {
+/*jslint strict: false */
+/*global $: false, document: false, window: false,
+  Hyphenator: false, alert: false */
+
+(function () {
+
+    var template,
+        hasInfo = false,
+        hasText = true,
+        data = {},
+        templateRegExp = /\{(\w+)\}/g;
 
     // nice easing
-    $.extend( $.easing, {
+    $.extend($.easing, {
         easeOutExpo: function (x, t, b, c, d) {
-            return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
-        },
-    });
-    
-    // size the pages
-    $(window).bind("load resize", function() {
-        var h = $(window).height();
-        $("section, .wrapper, .titleInfo").css({ "height" : h });
-        $("section.footer, section.footer > .wrapper, .footerInfo").css({ "height" : (h/2) });
-        $("article, .articleFlow").css({ "height" : (h-148) });
-    });
-    
-    // find width of the story
-/*
-    $(window).bind("load", function() {
-        alert($("#scrollWidth").attr("scrollWidth"));
-    });
-*/
-    
-    // find all sections and scroll to them 
-    $("nav a.next").click(function() {
-        var scrollTop = $(window).scrollTop();
-            $('section').each(function(i, section){ 
-                sectiontop = $(section).offset().top;
-                if (scrollTop < sectiontop) {
-                    $.scrollTo(section, 400, {easing:'easeOutExpo'});
-                return false;
-            } 
-        });
-    });
-
-    $.fn.reverse = function() {
-    return this.pushStack(this.get().reverse(), arguments);
-    };
-
-    $("nav a.prev").click(function(){
-        var scrollTop = $(window).scrollTop();
-            $('section').reverse().each(function(i, section){
-                sectiontop = $(section).offset().top;
-                if (scrollTop > sectiontop) { 
-                    $.scrollTo(section, 400, {easing:'easeOutExpo'});
-                return false;
-            }
-        });
-    });
-    
-    // calculate total pages
-    var totalPages = $("section.page").size();
-    $(".totalPages").text(totalPages);
-    
-    // calculate current page number
-    $(".pageNumber").each(function(i){
-        $(this).prepend((i + 1));
-    });
-    
-    // keyboard navigation between sections
-    $(document.documentElement).keyup(function (event) {
-        if (event.keyCode == 74) {
-            $("nav a.next").click(); 
-        } else if (event.keyCode == 75) {
-            $("nav a.prev").click(); 
+            return (t === d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
         }
     });
-    
-    // hyphenate
-    	Hyphenator.config({
-		displaytogglebox : true,
-		classname: 'articleFlow',
-		orphancontrol: 2,
-		minwordlength : 4
-	});
-	Hyphenator.run();
-   
-    
-    // and we're done
-    
-});
 
+    // helper function to reverse order of elements.
+    $.fn.reverse = function () {
+        return this.pushStack(this.get().reverse(), arguments);
+    };
+
+    function prepTemplate() {
+        // parse out the template
+        var templateNode = document.getElementById('template');
+
+        template = templateNode.text.trim();
+
+        // remove the template node from the DOM, since no longer needed.
+        templateNode.parentNode.removeChild(templateNode);
+    }
+
+    function applyTemplate(pageData) {
+        // add page data to main data. A bit hazardous since properties
+        // are changed, but OK since it is the same fields each time.
+        // revisit longer term.
+        $.extend(data, pageData);
+
+        return template.replace(templateRegExp, function (match, prop) {
+            return data[prop] || '';
+        });
+    }
+
+    function sizeContent() {
+        var h = $(window).height();
+        $("section, .wrapper, .titleInfo").css({ "height" : h });
+        $("section.footer, section.footer > .wrapper, .footerInfo").css({ "height" : (h / 2) });
+        $("article, .articleFlow").css({ "height" : (h - 148) });
+    }
+
+
+    function onResize() {
+        sizeContent();
+    }
+
+    // inject the HTML for the book content. Call this
+    // after the page has loaded.
+    function injectContent() {
+        var footerDom = $('.footer'),
+            text, totalPages, totalWidth, visibleWidth;
+
+        // inject the first copy of the story to figure out sizes/page numbers.
+        text = applyTemplate({
+            offset: 0,
+            pageNumber: 1,
+            pageClasses: 'newChapter'
+        });
+        footerDom.before(text);
+        sizeContent();
+
+        // find width of the story
+        totalWidth = $(".scrollWidth").attr("scrollWidth");
+        // hmm where does the 10 come from?
+        pageWidth = $(".scrollWidth").outerWidth() + 20;
+
+        totalPages = Math.ceil(totalWidth / pageWidth);
+
+        // generate the rest of the pages
+        text = '';
+        for (i = 1; i < totalPages; i++) {
+            text += applyTemplate({
+                offset: -(i * pageWidth),
+                pageNumber: i + 1,
+                pageClasses: ''
+            });
+        }
+
+        footerDom.before(text);
+        sizeContent();
+
+        // update total pages
+        $(".totalPages").text(totalPages);
+
+        // hyphenate
+        Hyphenator.config({
+            displaytogglebox : true,
+            classname: 'articleFlow',
+            orphancontrol: 2,
+            minwordlength : 4
+        });
+        Hyphenator.run();
+    }
+
+    function init() {
+        prepTemplate();
+
+        $(window)
+            // size the pages
+            .bind("resize", sizeContent)
+            // inject the content
+            .bind("load", injectContent);
+
+        // find all sections and scroll to them
+        $("nav a.next").click(function () {
+            var scrollTop = $(window).scrollTop();
+            $('section').each(function (i, section) {
+                var sectiontop = $(section).offset().top;
+                if (scrollTop < sectiontop) {
+                    $.scrollTo(section, 400, {easing: 'easeOutExpo'});
+                    return false;
+                }
+            });
+        });
+
+        $("nav a.prev").click(function () {
+            var scrollTop = $(window).scrollTop();
+            $('section').reverse().each(function (i, section) {
+                var sectiontop = $(section).offset().top;
+                if (scrollTop > sectiontop) {
+                    $.scrollTo(section, 400, {easing: 'easeOutExpo'});
+                    return false;
+                }
+            });
+        });
+
+        // keyboard navigation between sections
+        $(document.documentElement).keyup(function (event) {
+            if (event.keyCode === 74) {
+                $("nav a.next").click();
+            } else if (event.keyCode === 75) {
+                $("nav a.prev").click();
+            }
+        });
+    }
+
+    function checkComplete() {
+        if (hasInfo && hasText) {
+            init();
+        }
+    }
+
+    //Load the book info.
+    $.ajax('info.json', {
+        dataType: 'json',
+        success: function (info, textStatus, jqXhr) {
+            $.extend(data, info);
+            hasInfo = true;
+            checkComplete();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('on noes. an error getting info! ' + errorThrown);
+        }
+    });
+
+    //Load the book content.
+    $.ajax('content.html', {
+        dataType: 'text',
+        success: function (text, textStatus, jqXhr) {
+            data.content = text;
+            hasText = true;
+            checkComplete();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('on noes. an error getting content! ' + errorThrown);
+        }
+    });
+}());
